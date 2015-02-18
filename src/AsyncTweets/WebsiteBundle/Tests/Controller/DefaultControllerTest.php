767c93714f5435dcf4c622f491a5e11f6c2591e5
Alexis Lefebvre <alexislefebvre@gmail.com>
2015-02-18 20:59:46 +0100

    Added fixtures Added DoctrineFixturesBundle and LiipFunctionalTestBundle Restored WebsiteBundle tests

--- src/AsyncTweets/WebsiteBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AsyncTweets/WebsiteBundle/Tests/Controller/DefaultControllerTest.php
@@ -2,7 +2,7 @@
 
 namespace AsyncTweets\WebsiteBundle\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Liip\FunctionalTestBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
@@ -17,20 +17,55 @@ class DefaultControllerTest extends WebTestCase
     
     public function testIndex()
     {
+        $this->loadFixtures(array(
+            'AsyncTweets\TweetBundle\DataFixtures\ORM\LoadUserData',
+            'AsyncTweets\TweetBundle\DataFixtures\ORM\LoadTweetData',
+            'AsyncTweets\TweetBundle\DataFixtures\ORM\LoadMediaData',
+        ));
+        
         $path = $this->router->generate('asynctweets_website_homepage');
         
         $crawler = $this->client->request('GET', $path);
-
+        
+        # <body>
         $this->assertEquals(1,
             $crawler->filter('html > body')->count());
-            
-        //~ $this->assertEquals(1,
-            //~ $crawler->filter('title:contains("Home timeline - AsyncTweets")')->count());
         
-        //~ $this->assertEquals(1,
-            //~ $crawler->filter('div.tweets')->count());
+        # <title>
+        $this->assertEquals(1,
+            $crawler->filter('title:contains("Home timeline - AsyncTweets")')->count());
         
-        //~ $this->assertGreaterThanOrEqual(1,
-            //~ $crawler->filter('main.container > div.tweets > div.media')->count());            
+        # 2 navigation blocks
+        $this->assertEquals(2,
+            $crawler->filter('main.container > div.navigation')->count());
+        
+        # Tweet
+        $this->assertEquals(1,
+            $crawler->filter(
+                'main.container > div.tweets > div.media > blockquote.media-body'
+            )->count());
+        
+        # Link
+        $this->assertEquals(1,
+            $crawler->filter(
+                'main.container > div.tweets > div.media > blockquote.media-body > '.
+                'p > a'
+            )->count());
+        
+        # TODO: Hashtags
+        
+        # Image
+        $this->assertEquals(1,
+            $crawler->filter('main.container > div.tweets > div.media')->count());
+        
+        $this->assertEquals(2,
+            $crawler->filter(
+                'blockquote.media-body > p')->count());
+        
+        # User
+        $this->assertEquals(1,
+            $crawler->filter(
+                'main.container > div.tweets > div.media > blockquote.media-body > small > a:contains("Asynchronous tweets")'
+            )->count());
     }
 }
